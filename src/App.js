6ec775ffecf97cf3ac2d8113aaf63a9a40bcae91
Damini613 <kumaridamini44@gmail.com>
2022-05-26 18:37:52 +0530

    init

--- src/App.js
+++ src/App.js
@@ -1,25 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from 'react';
+import ReactGa from 'react-ga'
+import './App.css'
+class App extends Component {
+  componentDidMount() {
+    ReactGa.initialize('UA-229963861-1')
+    //page view
+    ReactGa.pageview('/')
+    // ReactGa.onClick('')
+  }
+  
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  show(){
+    console.log('m clicked')
+    alert('m clicked')
+  }
+  render() {
+    return (
+      <div>
+        <h2>Hello World</h2>
+    <h3><a href="www.google.com">Google.com</a></h3>
+    <button className = "show" onClick={()=>this.show()}>Click Me</button>
+      </div>
+    );
+  }
 }
 
 export default App;
